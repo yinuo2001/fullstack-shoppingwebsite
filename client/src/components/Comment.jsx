23c8cf1f422a2c6e02a883ee368f09137fbcd37d
Tia-Guo <guo.zil@northeastern.edu>
2024-07-29 19:36:55 -0700

    Add the comment page

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../css/Comment.css";
+
+const Comments = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await axios.get("/api/comments");
+        setComments(response.data);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+
+    fetchComments();
+  }, []);
+
+  const handlePostComment = async () => {
+    if (isAuthenticated) {
+      try {
+        await axios.post("/api/comments", { comment: newComment });
+        setNewComment("");
+        const response = await axios.get("/api/comments");
+        setComments(response.data);
+      } catch (error) {
+        console.error("Error posting comment:", error);
+      }
+    } else {
+      loginWithRedirect();
+    }
+  };
+
+  return (
+    <div className="comment">
+
+      {/* Header */}
+      <header className="header">
+        <Link to="/" className="main-header">Style Haven</Link>
+      </header>
+
+      {/* Comment Section */}
+      <header className="comment-header">
+        <h2>Comments</h2>
+      </header>
+
+      <div className="comment-input">
+        <textarea
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Please log in to comment..."
+        ></textarea>
+        <button onClick={handlePostComment}>Post</button>
+      </div>
+      <hr />
+      <div className="comments-list">
+        {comments.map((comment, index) => (
+          <div key={index} className="comment-item">
+            <p><strong>{comment.user}</strong>: {comment.text}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
