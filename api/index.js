7c25bd8d6af4732df7dad9f10c95a2e27d068795
eliseven-7 <feng.yinu@northeastern.edu>
2024-08-01 01:40:20 +0800

    Implemented CRUD functionalities for three tables

--- api/index.js
+++ api/index.js
@@ -28,6 +28,124 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+// Deals with Product table
+// Only Auth0 users can search for products
+app.get("/products/:name", requireAuth, async (req, res) => {
+  const { name } = req.params;
+  const products = await prisma.product.findMany({
+    where: {
+      title: {
+        contains: name,
+      },
+    },
+  });
+  res.json(products);
+});
+
+// A list of all products are displayed to all users
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+});
+
+// Deals with Comment table
+// All users can see all comments in this website
+app.get("/comments", async (req, res) => {
+  const comments = await prisma.comment.findMany();
+  res.json(comments);
+});
+
+// Auth0 users can add a comment
+app.post("/comments", requireAuth, async (req, res) => {
+  const { text } = req.body;
+  const auth0Id = req.auth.payload.sub;
+  const user = await prisma.user.findUnique({
+    where: {
+      auth0Id,
+    },
+  });
+
+  const comment = await prisma.comment.create({
+    data: {
+      userId: user.id,
+      text,
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  res.json(comment);
+});
+
+// Auth0 users can delete their own comments
+app.delete("/comments/:id", requireAuth, async (req, res) => {
+  const { id } = req.params;
+  const auth0Id = req.auth.payload.sub;
+
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  if (comment.user.auth0Id !== auth0Id) {
+    return res.status(403).json({ error: "You can't delete this comment" });
+  }
+
+  await prisma.comment.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  res.json({ message: "Comment deleted" });
+});
+
+// Deals with User table
+// Auth0 users can get their own user information
+app.get("/verify-user", requireAuth, async (req, res) => {
+  const auth0Id = req.auth.payload.sub;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      auth0Id,
+    },
+  });
+
+  res.json(user);
+});
+
+// Auth0 users can update their user information
+app.put("/verify-user", requireAuth, async (req, res) => {
+  const auth0Id = req.auth.payload.sub;
+  const { email, name } = req.body;
+  const user = await prisma.user.update({
+    where: {
+      auth0Id,
+    },
+    data: {
+      email,
+      name,
+    },
+  });
+  res.json(user);
+});
+
+// Auth0 users can delete their account
+app.delete("/verify-user", requireAuth, async (req, res) => {
+  const auth0Id = req.auth.payload.sub;
+  const user = await prisma.user.delete({
+    where: { auth0Id },
+  });
+  res.json(user);
+});
+
 // this endpoint is used by the client to verify the user status and to make sure the user is registered in our database once they signup with Auth0
 // if not registered in our database we will create it.
 // if the user is already registered we will return the user information
