aa466b311300d1fc650c6f17c1e03a55b00f02d9
Tia-Guo <guo.zil@northeastern.edu>
2024-08-03 14:55:05 -0700

    Slightly change

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import Topbar from "./Topbar";
 
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -80,45 +81,13 @@ const Home = () => {
 
   return (
     <div className="home">
-      {/* Top Bar */}
-      <div className="topbar">
-        <div className="topbar-content">
-          <div className="location">
-            <i className="fas fa-map-marker-alt"></i>
-            <span>{location}</span>
-          </div>
-          <ul className="shortcut-menu">
-            <li>
-              <div className="search-bar">
-                <input type="text" placeholder="Search..." />
-                <button className="search-btn">
-                <i className="fas fa-search"></i>
-                </button>
-              </div>
-            </li>
-            {!isAuthenticated ? (
-              <li><button className="login-btn" onClick={() => loginWithRedirect()}>
-                <i className="fas fa-user"></i>
-                <span className="login-text">Login</span>
-                </button>
-              </li>
-            ) : (
-              <>
-                <li><span>Hello, {user.given_name}</span></li>
-                <li><button className="logout-btn" onClick={() => logout({ returnTo: window.location.origin })}>Logout</button></li>
-                <li><Link to="/profile" className="profile-link">Profile</Link></li>
-              </>
-            )}
-            <li className="shopping-bag">
-            <a href="#">
-                <i className="fas fa-shopping-bag"></i>
-                <span className="bag-count">{cartCount}</span> {/* Display cart count */}
-                <span className="tooltip">Shopping Bag</span>
-              </a>
-            </li> 
-          </ul>
-        </div>
-      </div>
+      <Topbar 
+        location={location} 
+        cartCount={cartCount} 
+        loginWithRedirect={loginWithRedirect} 
+        logout={logout} 
+        user={user}
+      />
 
       {/* Header */}
       <header className="header">
