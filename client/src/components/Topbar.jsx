e2c1c29160770b3a5025572e4702ee41494cf029
eliseven-7 <feng.yinu@northeastern.edu>
2024-08-06 19:56:18 +0800

    changed a href to link to

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -34,11 +34,11 @@ const Topbar = ({ location, cartCount, loginWithRedirect, logout, user }) => {
             </>
           )}
           <li className="shopping-bag">
-            <a href="/shopping-cart">
+            <Link to="/shopping-cart" className="shopping-bag-link">
               <i className="fas fa-shopping-bag"></i>
               <span className="bag-count">{cartCount}</span> {/* Display cart count */}
               <span className="tooltip">Shopping Bag</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
