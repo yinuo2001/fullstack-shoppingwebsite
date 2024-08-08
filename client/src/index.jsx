2a39b59acfd9a17711bf13bd427af53f54f71a70
Tia-Guo <guo.zil@northeastern.edu>
2024-08-08 16:08:54 -0700

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { AuthTokenProvider } from "./AuthTokenContext";
 import { requestedScopes } from "./constants";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
@@ -21,5 +23,4 @@ ReactDOM.render(
       </AuthTokenProvider>
     </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
